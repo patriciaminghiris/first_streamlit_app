a23f565fd61e68e9bafea30fc9c459caceca08f5
Etta <ettajam3s@gmail.com>
2022-07-12 00:48:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,4 @@ streamlit.dataframe(my_data_rows)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 add_my_fruit= streamlit.text_input('What fruit would you like information about?','Banana')
+streamlit.write('Thank you for entering', add_my_fruit)
